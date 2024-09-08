4a99bf7a0086b7be5e12c354be008d525e8302a2
DominicHuynh <phucvbin2005@gmail.com>
2024-09-08 17:51:24 +0930

    prac6

---
+++ Node.h
@@ -0,0 +1,14 @@
+
+#pragma once
+class Node {
+private:
+    int data;
+    Node* link;
+public:
+    Node();
+    Node(int data);
+    void addLink(Node* node);
+    Node* getNext();
+    int getData();
+    void setData(int data);
+};
